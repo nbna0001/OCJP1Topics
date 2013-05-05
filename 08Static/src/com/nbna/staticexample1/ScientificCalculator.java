7af2bc96fb239ec27840634ead04db84ddc5d099
sai haridass <sharidass1408@gmail.com>
2013-05-05 09:12:37 -0400

    testing

---
+++ 08Static/src/com/nbna/staticexample1/ScientificCalculator.java
@@ -0,0 +1,12 @@
+package com.nbna.staticexample1;
+
+public class ScientificCalculator extends Calculator{
+	
+	
+
+	public ScientificCalculator() {
+		
+		System.out.println("Scientific Cal is at your service");
+				
+	}
+}
